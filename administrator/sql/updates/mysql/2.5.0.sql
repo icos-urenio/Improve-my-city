2652d4f4838599aa84e4c020d6a201dee45b516d
Ioannis Tsampoulatidis <tsampoulatidis@gmail.com>
2012-09-16 22:51:57 +0300

    Fix autoupdate create new table

--- administrator/sql/updates/mysql/2.5.0.sql
+++ administrator/sql/updates/mysql/2.5.0.sql
@@ -1,10 +1,9 @@
 #2.5.0
- 
-CREATE TABLE IF NOT EXISTS `#__improvemycity_timestamp` (
+DROP TABLE IF EXISTS `#__improvemycity_timestamp`; 
+CREATE TABLE `#__improvemycity_timestamp` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `triggered` varchar(255),
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT COLLATE=utf8_general_ci;
-
 INSERT INTO `#__improvemycity_timestamp` (`triggered`) VALUES (MD5(RAND()));
