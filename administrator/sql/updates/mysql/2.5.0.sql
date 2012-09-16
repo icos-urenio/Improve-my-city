b35ecf3b85240a6bfa438ed763e22fe493c1e5f1
Ioannis Tsampoulatidis <tsampoulatidis@gmail.com>
2012-09-16 20:35:59 +0300

    added 2.5.0 sql for timestamp table

---
+++ administrator/sql/updates/mysql/2.5.0.sql
@@ -0,0 +1,10 @@
+#version 2.4.1 introduces timestamp table to be used by native android application
+ 
+CREATE TABLE IF NOT EXISTS `#__improvemycity_timestamp` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `triggered` varchar(255),
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT COLLATE=utf8_general_ci;
+
+INSERT INTO `#__improvemycity_timestamp` (`triggered`) VALUES (MD5(RAND()));
