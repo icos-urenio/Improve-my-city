5458d152b1d96add489f728b89ea8d5a79dcfe23
Ioannis Tsampoulatidis <tsampoulatidis@gmail.com>
2012-10-20 03:32:23 +0300

    bug with user name not being displayed on print view is fixed

--- site/views/issue/tmpl/default_print.php
+++ site/views/issue/tmpl/default_print.php
@@ -49,7 +49,7 @@ defined('_JEXEC') or die;
 	<div id="imc-issue-general-info">
 		<span class="strong"><?php echo JText::_('CATEGORY');?></span><span class="desc"><?php echo $this->item->catname;?></span><br />
 		<span class="strong"><?php echo JText::_('ADDRESS');?></span><span class="desc"><?php echo $this->item->address;?></span><br />
-		<span class="strong"><?php echo JText::_('REPORTED_BY');?></span><span class="desc"><?php echo $this->item->username . ' ' . $this->item->reported_rel;?></span><br />
+		<span class="strong"><?php echo JText::_('REPORTED_BY');?></span><span class="desc"><?php echo $this->item->fullname . ' ' . $this->item->reported_rel;?></span><br />
 		<span class="strong"><?php echo JText::_('VIEWED');?></span><span class="desc"><?php echo $this->item->hits;?></span><br />
 		<span class="strong"><?php echo JText::_('ISSUE_STATUS');?></span><span class="status-<?php echo $status;?>"><?php echo JText::_($status);?></span><br />						
 		<span class="strong"><?php echo JText::_('ISSUE_VOTES_DESC');?>: </span><span class="desc"><?php echo $this->item->votes;?></span>		
