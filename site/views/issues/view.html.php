d5418f0483900cf882cee53e837aa939b4d1d6e1
Panagiotis Tsarchopoulos <patsar@auth.gr>
2013-05-28 22:20:24 +0300

    Update view.html.php

--- site/views/issues/view.html.php
+++ site/views/issues/view.html.php
@@ -2,9 +2,9 @@
 /**
  * @version     2.5.x
  * @package     com_improvemycity
- * @copyright   Copyright (C) 2011 - 2012 URENIO Research Unit. All rights reserved.
+ * @copyright   Copyright (C) 2011 - 2013 URENIO Research Unit. All rights reserved.
  * @license     GNU Affero General Public License version 3 or later; see LICENSE.txt
- * @author      Ioannis Tsampoulatidis for the URENIO Research Unit
+ * @author      Panagiotis Tsarchopoulos for the URENIO Research Unit
  */
 
 
